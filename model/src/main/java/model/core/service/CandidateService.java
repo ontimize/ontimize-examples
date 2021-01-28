a573a3dd4a20c02c2ebe1c6b6d26dcefad12cb9f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2021-01-28 15:53:13 +0100

    Mail system example

---
+++ model/src/main/java/model/core/service/CandidateService.java
@@ -0,0 +1,75 @@
+package model.core.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
+import org.springframework.stereotype.Service;
+
+import com.ontimize.db.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEEException;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.services.mail.IMailService;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+
+import api.core.service.ICandidateService;
+import model.core.dao.CandidateDao;
+
+@Service("CandidateService")
+@Lazy
+public class CandidateService implements ICandidateService {
+
+	@Autowired
+	private CandidateDao candidateDao;
+	@Autowired
+	private DefaultOntimizeDaoHelper daoHelper;
+	@Autowired
+	private IMailService mailService;
+
+	@Override
+	public EntityResult candidateQuery(Map<String, Object> keyMap, List<String> attrList)
+			throws OntimizeJEERuntimeException {
+		return this.daoHelper.query(this.candidateDao, keyMap, attrList);
+	}
+
+	@Override
+	public EntityResult candidateInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+		EntityResult toRet = this.daoHelper.insert(this.candidateDao, attrMap);
+
+		if ((toRet.getCode() != EntityResult.OPERATION_WRONG)) {
+
+			Runnable runnable = () -> {
+
+				List<String> receiverList = new ArrayList<String>();
+				receiverList.add("receiver@example.com");
+				StringBuilder builder = new StringBuilder();
+				builder.append("Created new user.");
+				try {
+					this.mailService.sendMailWithoutAttach("my.mail@example.com", receiverList, "New candidate",
+							builder.toString());
+				} catch (OntimizeJEEException e) {
+				}
+			};
+			
+			DelegatingSecurityContextRunnable wrappedRunnable = new DelegatingSecurityContextRunnable(runnable);
+			new Thread(wrappedRunnable).start();
+		}
+
+		return toRet;
+	}
+
+	@Override
+	public EntityResult candidateUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+			throws OntimizeJEERuntimeException {
+		return this.daoHelper.update(this.candidateDao, attrMap, keyMap);
+	}
+
+	@Override
+	public EntityResult candidateDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+		return this.daoHelper.delete(this.candidateDao, keyMap);
+	}
+
+}
---
+++ ws/src/main/java/ws/core/rest/CandidateRestController.java
@@ -0,0 +1,24 @@
+package ws.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ontimize.jee.server.rest.ORestController;
+
+import api.core.service.ICandidateService;
+
+@RestController
+@RequestMapping("/candidates")
+@ComponentScan(basePackageClasses = { api.core.service.ICandidateService.class })
+public class CandidateRestController extends ORestController<ICandidateService> {
+
+	@Autowired
+	private ICandidateService candidateService;
+
+	@Override
+	public ICandidateService getService() {
+		return this.candidateService;
+	}
+}
