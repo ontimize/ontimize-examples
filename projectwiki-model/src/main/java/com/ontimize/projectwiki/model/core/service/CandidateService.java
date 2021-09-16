44e4ad99df6e415431397523057965d70a534734
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-09-16 11:46:35 +0200

    Mail system example

--- projectwiki-model/src/main/java/com/ontimize/projectwiki/model/core/service/CandidateService.java
+++ projectwiki-model/src/main/java/com/ontimize/projectwiki/model/core/service/CandidateService.java
@@ -1,14 +1,18 @@
 package com.ontimize.projectwiki.model.core.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
 import org.springframework.stereotype.Service;
 
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEEException;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.services.mail.IMailService;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import com.ontimize.projectwiki.api.core.service.ICandidateService;
 import com.ontimize.projectwiki.model.core.dao.CandidateDao;
@@ -21,7 +25,8 @@ public class CandidateService implements ICandidateService {
 	private CandidateDao candidateDao;
 	@Autowired
 	private DefaultOntimizeDaoHelper daoHelper;
-
+	@Autowired
+	private IMailService mailService;
 
 	@Override
 	public EntityResult candidateQuery(Map<String, Object> keyMap, List<String> attrList)
@@ -31,7 +36,26 @@ public class CandidateService implements ICandidateService {
 
 	@Override
 	public EntityResult candidateInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-		return this.daoHelper.insert(this.candidateDao, attrMap);
+		EntityResult toRet = this.daoHelper.insert(this.candidateDao, attrMap);
+
+		if ((toRet.getCode() != EntityResult.OPERATION_WRONG)) {
+			Runnable runnable = () -> {
+				List<String> receiverList = new ArrayList<String>();
+				receiverList.add("receiver@example.com");
+				StringBuilder builder = new StringBuilder();
+				builder.append("Created new user.");
+				try {
+					this.mailService.sendMailWithoutAttach("my.mail@example.com", receiverList, "New candidate",
+							builder.toString());
+				} catch (OntimizeJEEException e) {
+				}
+			};
+
+			DelegatingSecurityContextRunnable wrappedRunnable = new DelegatingSecurityContextRunnable(runnable);
+			new Thread(wrappedRunnable).start();
+		}
+
+		return toRet;
 	}
 
 	@Override
