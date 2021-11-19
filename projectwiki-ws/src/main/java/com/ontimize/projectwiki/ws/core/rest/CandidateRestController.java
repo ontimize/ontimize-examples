4bbf01dd8e4b06c1c139d3c0c00f057a8eeb0d3d
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-11-19 13:56:55 +0100

    Initial project

---
+++ projectwiki-ws/src/main/java/com/ontimize/projectwiki/ws/core/rest/CandidateRestController.java
@@ -0,0 +1,23 @@
+package com.ontimize.projectwiki.ws.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ontimize.jee.server.rest.ORestController;
+import com.ontimize.projectwiki.api.core.service.ICandidateService;
+
+@RestController
+@RequestMapping("/candidates")
+@ComponentScan(basePackageClasses = { com.ontimize.projectwiki.api.core.service.ICandidateService.class })
+public class CandidateRestController extends ORestController<ICandidateService> {
+
+ @Autowired
+ private ICandidateService candidateService;
+
+ @Override
+ public ICandidateService getService() {
+  return this.candidateService;
+ }
+}
