177fd7fcf1827570d32d475afd88f6d232e8a8c7
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2023-06-20 11:38:40 +0200

    application title changed

--- projectwiki-frontend/src/app/app.config.ts
+++ projectwiki-frontend/src/app/app.config.ts
@@ -12,10 +12,10 @@ export const CONFIG: Config = {
   // Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app.
   // By default is set as 'ontimize-web-uuid'.
-  uuid: 'com.ontimize.web.ngx.jee.seed',
+  uuid: 'com.ontimize.web.examplepermission',
 
   // Title of the app
-  title: 'JEE seed',
+  title: 'Permission example',
 
   //  Language of the application.
   locale: 'en',
