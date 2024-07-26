9d3e1fbfa6f82f594d4ad2cf2f15985b7ca911e8
Xoán Loureiro Santamaría <xoan.loureiro@imatia.com>
2024-07-26 08:17:44 +0200

    OpenAPI System example

--- projectwiki-ws/src/main/java/com/ontimize/projectwiki/ws/core/rest/UserRestController.java
+++ projectwiki-ws/src/main/java/com/ontimize/projectwiki/ws/core/rest/UserRestController.java
@@ -1,22 +1,22 @@
 package com.ontimize.projectwiki.ws.core.rest;
 
 
+import com.ontimize.projectwiki.openapi.core.service.IUsersApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ontimize.projectwiki.api.core.service.IUserService;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.server.rest.ORestController;
 
-
 @RestController
 @RequestMapping("/users")
-public class UserRestController extends ORestController<IUserService> {
+public class UserRestController extends ORestController<IUserService> implements IUsersApi {
 
 	@Autowired
 	private IUserService userSrv;
@@ -26,10 +26,11 @@ public class UserRestController extends ORestController<IUserService> {
 		return this.userSrv;
 	}
 
-	@RequestMapping(
+	/* @RequestMapping(
 		value = "/login",
 		method = RequestMethod.POST,
-		produces = MediaType.APPLICATION_JSON_VALUE)
+		produces = MediaType.APPLICATION_JSON_VALUE) */
+	@Override
 	public ResponseEntity<EntityResult> login() {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
